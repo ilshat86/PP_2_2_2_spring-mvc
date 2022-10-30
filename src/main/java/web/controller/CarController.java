e6d4471e884ec127ceca38e86ef04208651e98d4
ilshat <isibagatullin@infostart.ru>
2022-10-30 23:37:37 +0500

    Задание исправлено

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,24 +1,29 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.dao.CarDao;
 import web.model.Car;
-import web.service.CarService;
-import web.service.CarServiceImp;
 
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    CarService carService = new CarServiceImp();
+    private final CarDao carDao;
+
+    @Autowired
+    public CarController(CarDao carDao) {
+        this.carDao = carDao;
+    }
 
     @RequestMapping(value = "/cars", method = RequestMethod.GET)
     public String getCars(ModelMap model, @RequestParam(required = false, defaultValue = "5", name = "count") int count) {
-        List<Car> cars = carService.listCars(count);
+        List<Car> cars = carDao.listCar(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
--- src/main/java/web/dao/CarDao.java
+++ src/main/java/web/dao/CarDao.java
@@ -1,10 +1,31 @@
 package web.dao;
 
+import org.springframework.stereotype.Component;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface CarDao {
-    List<Car> listCar(int count);
+@Component
+public class CarDao {
+
+    private List<Car> cars;
+
+    {
+        cars = new ArrayList<>();
+
+        cars.add(new Car("ford", "focus", 1));
+        cars.add(new Car("ford", "focus", 2));
+        cars.add(new Car("ford", "focus", 3));
+        cars.add(new Car("toyota", "mark", 1));
+        cars.add(new Car("toyota", "mark", 2));
+        cars.add(new Car("toyota", "prado", 4));
+    }
+
+    public List<Car> listCar(int count) {
+        if (count < 5) {
+            return cars.subList(0, count);
+        } else return cars;
+    }
 
 }
